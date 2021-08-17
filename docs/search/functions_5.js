e1347945c802a18fae2862e99f530f1a8bd255b9
coveobot <coveobot@coveo.com>
2021-08-17 15:26:17 +0000

    Update documentation

--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['find_120',['find',['../classcachemere_1_1Cache.html#a2075579728f01a9b5e2f83d4e1d20355',1,'cachemere::Cache']]]
+  ['find_120',['find',['../classcachemere_1_1Cache.html#a563b737e0c1e791cf7e0343d545d8437',1,'cachemere::Cache']]]
 ];
