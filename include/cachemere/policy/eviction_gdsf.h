30b4906488484ebe811de727daa61d566358b062
William Dussault <wdussault@coveo.com>
2021-12-10 14:07:43 -0500

    Clarify cost function specification and bump version

--- include/cachemere/policy/eviction_gdsf.h
+++ include/cachemere/policy/eviction_gdsf.h
@@ -18,6 +18,8 @@ namespace cachemere::policy {
 /// @tparam Key The type of the keys used to identify items in the cache.
 /// @tparam Value The type of the values stored in the cache.
 /// @tparam Cost A functor taking a a `Key&` and a `const Item<Value>&` returning the cost to load this item in cache.
+//          The cost must not exceed 2^64 and should ideally be quite a bit below this limit since the cost of the item is added to the
+//          policy's internal 64-bit clock on every insertion.
 template<typename Key, typename Value, typename Cost> class EvictionGDSF
 {
 private:
