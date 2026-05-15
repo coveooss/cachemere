5f98ac9e8d361e1d2f26a0c50668145af8995902
William Dussault <wdussault@coveo.com>
2026-05-15 09:21:54 -0400

    Support `<=>` operator

--- include/cachemere/detail/transparent_eq.h
+++ include/cachemere/detail/transparent_eq.h
@@ -10,19 +10,29 @@ template<typename Key> struct TransparentEq {
 
     bool operator()(const Key& a, const Key& b) const
     {
-        return a == b;
+        return equal(a, b);
     }
 
     template<typename KeyView> bool operator()(const Key& a, const KeyView& b) const
     {
-        // We only require operator==(const Key&) to be defined on the KeyView.
-        return b == a;
+        // We only require equality or three-way comparison against `Key` to be defined on the KeyView.
+        return equal(b, a);
     }
 
     template<typename KeyView> bool operator()(const KeyView& a, const Key& b) const
     {
-        // We only require operator==(const Key&) to be defined on the KeyView.
-        return a == b;
+        // We only require equality or three-way comparison against `Key` to be defined on the KeyView.
+        return equal(a, b);
+    }
+
+private:
+    template<typename Lhs, typename Rhs> static bool equal(const Lhs& lhs, const Rhs& rhs)
+    {
+        if constexpr (requires { lhs == rhs; }) {
+            return lhs == rhs;
+        } else {
+            return (lhs <=> rhs) == 0;
+        }
     }
 };
 
